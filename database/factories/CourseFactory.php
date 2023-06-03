4fde9e9790e685fe7fc5f463d808f4e4e6f088e6
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2023-06-03 07:36:09 +0300

    seed data and factory data

---
+++ database/factories/CourseFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\AppModelsCourse>
+ */
+class CourseFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+           
+            'name' => fake(),
+            'teacher_name' => fake(),
+            'total_hours' => random_int(1, 100),
+            
+        ];
+    }
+}
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,6 +13,18 @@ class UsersTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        //two initial users for logging in tests
+        $users = [
+            [
+             'email' => 'testuser1@gmail.com',
+             'password' => 123456789,
+            ],[
+                'email' => 'testuser2@gmail.com',
+                'password' => 1234567890,
+            ]
+         ];
+        
+        
+         DB::table('users')->insert($users);
     }
 }
